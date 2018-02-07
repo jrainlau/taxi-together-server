a7b9b7cde30eb2d708089d84e5b887c5c2e5986f
JrainLau <jrainlau@163.com>
2018-02-07 18:15:04 +0800

    fix: fix route bug

--- router/index.js
+++ router/index.js
@@ -3,10 +3,10 @@ const Router = require('koa-router')
 const router = new Router()
 
 ;[
-  '/bill'
+  'bill'
 ].forEach((route) => {
-  const routeInstance = require(`.${route}.js`)
-  router.use(route, routeInstance.routes(), routeInstance.allowedMethods())
+  const routeInstance = require(`./${route}.js`)
+  router.use(`taxi/${route}`, routeInstance.routes(), routeInstance.allowedMethods())
 })
 
 module.exports = router
